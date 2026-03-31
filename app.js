9ea2111cd1b3ed73356f1acf553e649a73d2a02d
Alan Yudi <alan.yudi10@aluno.ifce.edu.br>
2026-03-31 19:42:13 -0300

    Atividade web

--- app.js
+++ app.js
@@ -2,30 +2,58 @@ const express = require("express")
 const app = express()
 const port = 3000
 
-//endpoints
-
-app.get("/", (req, res) =>{
+app.use((req, res, next) => {
+    console.log(`Acessou: ${req.url}`)
+    next()
+})
+function middlewareRota(nome) {
+    return (req, res, next) => {
+        console.log(`Middleware da rota: ${nome}`)
+        next()
+    }
+}
+app.get("/", middlewareRota("index"), (req, res) => {
     res.send("index")
 })
-app.get("/about", (req, res) =>{
+
+app.get("/about", middlewareRota("about"), (req, res) => {
     res.send("about")
 })
 
-app.get("/user/", (req, res) =>{
-    
+app.get("/signup", (req, res) => {
+    res.send("Página de cadastro (signup)")
 })
 
-app.get("/users/:id/:pwd", (req, res) =>{
-    const id = req.Params.id
-    const pwd = req.Params.pwd
-    res.send("/users/:id/:{pwd}")
+
+app.get("/signin/users/:userid?", (req, res) => {
+    const userid = req.params.userid
+
+    // se não tiver userid → redireciona (item 4)
+    if (!userid) {
+        return res.redirect("/signup")
+    }
+
+    res.send(`Bem-vindo, usuário ${userid}!`)
 })
 
-app.get("/*splat", (req, res) => {
-    res.statusCode(404).sebd("error")
+
+app.get("/users/:id/:pwd", (req, res) => {
+    const id = req.params.id
+    const pwd = req.params.pwd
+
+    res.send(`ID: ${id} | Senha: ${pwd}`)
 })
+
+
+app.get("/user", (req, res) => {
+    res.send("rota /user")
+})
+
+
+app.get("*", (req, res) => {
+    res.status(404).send("error")
+})
+
 app.listen(port, () => {
-    console.log("Ta rodando")
+    console.log("Tá rodando na porta " + port)
 })
-
-//
